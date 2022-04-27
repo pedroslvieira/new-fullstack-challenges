baebb648deb52959bf9a367ac4bf9dfe801d162e
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-04-27 11:38:07 +0200

    wagon race done

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,10 @@
 // TODO: write your code here
+
+const carOne = document.querySelector('#player1-race .active');
+document.addEventListener("keyup", (event) => {
+  if (event.key == "o") {
+    carOne.classList.toggle("active");
+  } else {
+    console.log("not the right key");
+  }
+});
